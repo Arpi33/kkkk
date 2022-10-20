8b594a8d15c6f0727175e28f37341ca7ddb74562
Arpita Bhattacharjee <71565732+Arpi33@users.noreply.github.com>
2022-10-21 02:22:18 +0800

    Update try.py

--- try.py
+++ try.py
@@ -4,30 +4,18 @@ Created on Wed Oct 19 23:44:50 2022
 
 @author: 60112
 """
-
 import streamlit as st
-def get_mood():
-    dic = {'Select your mood here':0, 'Fatigued':1,'Sleepy':2,'Hungry':3,'Angry':4,'Lonely':5,'Sad':6,'Happy':7,'Bored':8,\
-      'Overweight':9,'Underweight':9,'High Glucose':11,'Headache':12}
-    feel = []
-    for i in dic.keys():
-        feel.append(i)
-    return feel,dic
+from google.cloud import firestore
 
-def process_mood(mood):
-    
-    _,di=get_mood()
-    return di[mood]
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestore-key.json")
 
+# Create a reference to the Google post.
+doc_ref = db.collection("food").document("Google")
 
-#mood_list,_=get_mood()
-#st.selectbox("Select Your mood today", ['a','b'])
-#mood=st.radio("How are you feeling today?", mood_list)
-#st.write(process_mood(mood))
+# Then get the data at that reference.
+doc = doc_ref.get()
 
-selected = st.selectbox('Select one option:', ['', 'First one', 'Second one'], format_func=lambda x: 'Select an option' if x == '' else x)
-
-#if selected:
- #   st.success('Yay! 🎉')
-#else:
- #   st.warning('No option is selected')
+# Let's see what we got!
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
